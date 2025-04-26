e41a2e06d6d0cb346f32ba558e2574e5c9a9cbee
kirit <kamiku319@gmail.com>
2025-04-26 17:12:30 -0400

    Update build script and Dockerfile with additional labels and fix syntax

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 docker build -t example-app -f DockerFile .
+
 if [ $? -eq 0 ]; then
     echo "Docker image built successfully!"
 else
     echo "Docker build failed!"
     exit 1
-fi 
+fi
